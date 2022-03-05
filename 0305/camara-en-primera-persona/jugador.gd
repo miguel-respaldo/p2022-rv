5881c78764fd93babedf11f88b3be4c06dadd4b6
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2022-03-05 10:15:27 -0600

    nos movemos por el mundo

--- 0305/camara-en-primera-persona/jugador.gd
+++ 0305/camara-en-primera-persona/jugador.gd
@@ -5,6 +5,8 @@ extends KinematicBody
 export var h_sensibilidad = 0.0005
 export var v_sensibilidad = 0.001
 
+export var velocidad = 10
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -12,7 +14,25 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y( -event.relative.x * h_sensibilidad )
+		$Camera.rotate_x( -event.relative.y * v_sensibilidad )
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var vel_actual = Vector3(0, 0, 0)
+
+	# Velocidad hacia adelante y atras
+	if Input.is_action_pressed("ui_up"):
+		vel_actual.z -= velocidad
+	if Input.is_action_pressed("ui_down"):
+		vel_actual.z += velocidad
+
+	# Velocidad hacia izuierda y derecha
+	if Input.is_action_pressed("ui_left"):
+		vel_actual.x -= velocidad
+	if Input.is_action_pressed("ui_right"):
+		vel_actual.x += velocidad
+
+	vel_actual = vel_actual.rotated( Vector3(0, 1, 0), rotation.y)
+
+	# Esta linea se encarga de movernos
+	move_and_slide( vel_actual )
