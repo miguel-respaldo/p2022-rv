7d157fd555e67387dada8fb924a4c2b483013316
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2022-03-05 09:58:01 -0600

    capturamos mouse y vemos hacia los lados

--- 0305/camara-en-primera-persona/jugador.gd
+++ 0305/camara-en-primera-persona/jugador.gd
@@ -7,8 +7,11 @@ export var v_sensibilidad = 0.001
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y( -event.relative.x * h_sensibilidad )
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
